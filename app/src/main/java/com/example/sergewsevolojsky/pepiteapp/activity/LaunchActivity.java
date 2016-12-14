c06763b5bf04a38b3c3ccf5ca4b8dfa6fa645c33
Medved6 <swsevolojsky@gmail.com>
2016-12-14 22:31:18 +0100

    add launch view

--- app/src/main/java/com/example/sergewsevolojsky/pepiteapp/activity/LaunchActivity.java
+++ app/src/main/java/com/example/sergewsevolojsky/pepiteapp/activity/LaunchActivity.java
@@ -1,25 +1,15 @@
 package com.example.sergewsevolojsky.pepiteapp.activity;
 
-import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
 import com.example.sergewsevolojsky.pepiteapp.R;
 
-public class SplashActivity extends AppCompatActivity {
+public class LaunchActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_splash);
-
-
-
-
-
-
-        Intent intent = new Intent(this, LaunchActivity.class);
-        startActivity(intent);
-        finish();
+        setContentView(R.layout.activity_launch);
     }
 }
--- app/src/main/java/com/example/sergewsevolojsky/pepiteapp/activity/LoginActivity.java
+++ app/src/main/java/com/example/sergewsevolojsky/pepiteapp/activity/LoginActivity.java
@@ -20,40 +20,8 @@ public class LoginActivity extends AppCompatActivity {
 
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        Auth0 auth0 = new Auth0(getString(R.string.auth0_client_id), getString(R.string.auth0_domain));
-        lock = Lock.newBuilder(auth0, callback)
-                // Add parameters to the Lock Builder
-                .build(this);
-
-        startActivity(lock.newIntent(this));
+        setContentView(R.layout.activity_login);
     }
 
-    @Override
-    protected void onDestroy() {
-        super.onDestroy();
-        // Your own Activity code
-        lock.onDestroy(this);
-        lock = null;
-    }
 
-    private final LockCallback callback = new AuthenticationCallback() {
-
-
-        @Override
-        public void onAuthentication(Credentials credentials) {
-            Toast.makeText(getApplicationContext(), "Log In - Success", Toast.LENGTH_SHORT).show();
-            //startActivity(new Intent(getApplicationContext(), MainActivity.class));
-            finish();
-        }
-
-        @Override
-        public void onCanceled() {
-            Toast.makeText(getApplicationContext(), "Log In - Cancelled", Toast.LENGTH_SHORT).show();
-        }
-
-        @Override
-        public void onError(LockException error) {
-            Toast.makeText(getApplicationContext(), "Log In - Error Occurred", Toast.LENGTH_SHORT).show();
-        }
-    };
 }
